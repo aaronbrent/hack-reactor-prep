3832a5478db47087c02998c3fe1aa708c681fad8
Aaron Ellis <aaronbrentellis@gmail.com>
2017-06-08 12:30:02 -0600

    added more practice functions

--- app.js
+++ app.js
@@ -331,4 +331,203 @@ function cube(num) {
   return ( num * num * num );
 }
 
+//Write a function called "square".
+//
+//Given a number, "square" should return the square of the given number.
+//
+//var output = square(5);
+//console.log(output); // --> 25
+
+function square(num) {
+  return ( num * num );
+}
+
+//Write a function called "computeAverageLengthOfWords".
+//
+//Given two words, "computeAverageLengthOfWords" returns the average of their lengths.
+//
+//var output = computeAverageLengthOfWords('code', 'programs');
+//console.log(output); // --> 6
+
+function computeAverageLengthOfWords(word1, word2) {
+  return ( (word1.length + word2.length) / 2 );
+}
+
+//Write a function called "addFullNameProperty".
+//
+//Given an object that has a "firstName" property and a "lastName" property, "addFullNameProperty" returns a "fullName" property whose value is a string with the first name and last name separated by a space.
+//
+//var person = {
+//  firstName: 'Jade',
+//  lastName: 'Smith'
+//};
+//addFullNameProperty(person);
+//console.log(person.fullName); // --> 'Jade Smith'
+
+function addFullNameProperty(obj) {
+  obj.fullName = obj.firstName + ' ' + obj.lastName;
+  return obj;
+}
+
+//Write a function called "addObjectProperty".
+//
+//Given two objects and a key, "addObjectProperty" sets a new property on the 1st object at the given key. The value of that new property is the entire 2nd object.
+//
+//var person1 = {
+//  name: 'Joe Blow',
+//  role: 'schlub'
+//};
+//var person2 = {
+//  name: 'Mr. Burns',
+//  role: 'supervisor'
+//};
+//addObjectProperty(person1, 'manager', person2);
+//console.log(person1.manager); // --> { name: 'Mr. Burns', role: 'supervisor' }
+
+function addObjectProperty(obj1, key, obj2) {
+  obj1[key] = obj2;
+  return (obj1);
+}
+
+//Write a function called "isPersonOldEnoughToDrinkAndDrive".
+//
+//Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrinkAndDrive" returns whether the given person is old enough to legally drink and drive in the United States.
+//
+//Notes:
+//* The legal drinking age in the United States is 21.
+//* The legal driving age in the United States is 16.
+//* It is always illegal to drink and drive in the United States.
+//
+//var obj = {
+//  age: 45
+//};
+//var output = isPersonOldEnoughToDrinkAndDrive(obj);
+//console.log(output); // --> false
+
+function isPersonOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+//Write a function called "isPersonOldEnoughToDrive".
+//
+//Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrive" returns whether the given person is old enough to drive.
+//
+//Notes:
+//* The legal driving age in the United States is 16.
+//
+//var obj = {
+//  age: 16
+//};
+//var output = isPersonOldEnoughToDrive(obj);
+//console.log(output); // --> true
+
+function isPersonOldEnoughToDrive(person) {
+  return ( person.age >= 16 );
+}
+
+//Write a function called "isPersonOldEnoughToVote".
+//
+//Given a "person" object, that contains an "age" property, "isPersonOldEnoughToVote" returns whether the given person is old enough to vote.
+//
+//Notes:
+//* The legal voting age in the United States is 18.
+//
+//var obj = {
+//  age: 19
+//};
+//var output = isPersonOldEnoughToVote(obj);
+//console.log(output); // --> true
+
+function isPersonOldEnoughToVote(person) {
+  return ( person.age >= 18 );
+}
+
+//Write a function called "isPersonOldEnoughToDrink".
+//
+//Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrink" returns whether the given person is old enough to drink.
+//
+//Notes:
+//* The legal drinking age in the United States is 21.
+//
+//var obj = {
+//  age: 16
+//};
+//var output = isPersonOldEnoughToDrink(obj);
+//console.log(output); // --> false
+
+function isPersonOldEnoughToDrink(person) {
+  return ( person.age >= 21 );
+}
+
+//Write a function called "addArrayProperty".
+//
+//Given an object, a key, and an array, "addArrayProperty" sets a new property on the object at the given key, with its value set to the given array.
+//
+//var myObj = {};
+//var myArray = [1, 3];
+//addArrayProperty(myObj, 'myProperty', myArray);
+//console.log(myObj.myProperty); // --> [1, 3]
+
+function addArrayProperty(obj, key, arr) {
+  obj[key] = arr;
+  return obj;
+}
+
+//Write a function called "getNthElement".
+//
+//Given an array and an integer, "getNthElement" returns the element at the given integer, within the given array.
+//
+//Notes:
+//* If the array has a length of 0, it should return 'undefined'.
+//
+//var output = getNthElement([1, 3, 5], 1);
+//console.log(output); // --> 3
+
+function getNthElement(array, n) {
+  return array[n];
+}
+
+//Write a function called "getFirstElement".
+//
+//Given an array, "getFirstElement" returns the first element of the given array.
+//
+//Notes:
+//* If the given array has a length of 0, it should return undefined.
+//
+//var output = getFirstElement([1, 2, 3, 4, 5]);
+//console.log(output); // --> 1
+
+function getFirstElement(array) {
+  return array[0];
+}
+
+//Write a function called "getLastElement".
+//
+//Given an array, "getLastElement" returns the last element of the given array.
+//
+//Notes:
+//* If the given array has a length of 0, it should return 'undefined'.
+//
+//var output = getLastElement([1, 2, 3, 4]);
+//console.log(output); // --> 4
+
+function getLastElement(array) {
+  var output = array.pop();
+  return output;
+}
+
+//Write a function called "addToFront".
+//
+//Given an array and an element, "addToFront" adds the given element to the front of the given array, and returns the given array.
+//
+//Notes: 
+//* It should be the SAME array, not a new array.
+//
+//var output = addToFront([1, 2], 3);
+//console.log(output); // -> [3, 1, 2]
+
+function addToFront(arr, element) {
+  arr.unshift(element);
+  return arr;
+}
 
