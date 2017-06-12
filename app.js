ca9254d216abff4057b5b20de5e5b6d0f941d9cc
Aaron Ellis <aaronbrentellis@gmail.com>
2017-06-11 23:17:10 -0600

    added new practice algorithms

--- app.js
+++ app.js
@@ -855,3 +855,290 @@ function removeFromBack(arr) {
   return arr;
 }
 
+//Write a function called "or".
+//
+//Given 2 boolean expressions, "or" returns true or false, corresponding to the || operator.
+//
+//Notes:
+//* Do not use the || operator.
+//* Use ! and && operators instead.
+//
+//var output = or(true, false);
+//console.log(output); // --> true;
+
+function or(expression1, expression2) {
+  if (!expression1 && expression2){
+    return true;
+  }else if (expression1 && !expression2){
+    return true;
+  }else if (expression1 && expression2){
+    return true;
+  }
+  return false;
+}
+
+//Write a function called "isEitherEvenOrAreBoth7".
+//
+//Given two numbers, 'isEitherEvenOrAreBoth7' returns whether at least one of them is even, or, both of them are 7.
+//
+//var output = isEitherEvenOrAreBoth7(3, 7);
+//console.log(output); // --> false
+//
+//var output = isEitherEvenOrAreBoth7(2, 3);
+//console.log(output); // --> true
+
+function isEitherEvenOrAreBoth7(num1, num2) {
+  if(num1 % 2 === 0 || num2 % 2 === 0){
+    return true;
+  }else if (num1 === 7 && num2 === 7){
+    return true;
+  }
+  return false;
+}
+
+//Write a function called "isEitherEvenAndLessThan9".
+//
+//Given two numbers, 'isEitherEvenAndLessThan9' returns whether at least one of them is even, and, both of them are less than 9.
+//
+//var output = isEitherEvenAndLessThan9(2, 4);
+//console.log(output); // --> true
+//
+//var output = isEitherEvenAndLessThan9(72, 2);
+//console.log(output); // --> false
+
+function isEitherEvenAndLessThan9(num1, num2) {
+  if ( (num1 < 9 && num2 < 9) && (num1 % 2 === 0 || num2 % 2 === 0) ){
+    return true;
+  }
+  return false;
+}
+
+//Write a function called "extend".
+//
+//Given two objects, "extend" adds properties from the 2nd object to the 1st object.
+//
+//Notes:
+//* Add any keys that are not in the 1st object.
+//* If the 1st object already has a given key, ignore it (do not overwrite the property value).
+//* Do not modify the 2nd object at all.
+//
+//var obj1 = {
+//  a: 1,
+//  b: 2
+//};
+//var obj2 = {
+//  b: 4,
+//  c: 3
+//};
+//
+//extend(obj1, obj2);
+//
+//console.log(obj1); // --> {a: 1, b: 2, c: 3}
+//console.log(obj2); // --> {b: 4, c: 3}
+
+function extend(obj1, obj2) {
+  for (var key in obj2){
+    if (!obj1[key]){
+      obj1[key] = obj2[key];
+    }
+  }
+  return obj1;
+}
+
+//Write a function called "removeNumbersLargerThan".
+//
+//Given a number and an object, "removeNumbersLargerThan" removes any properties whose values are numbers greater than the given number.
+//
+//var obj = {
+//  a: 8,
+//  b: 2,
+//  c: 'montana'
+//}
+//removeNumbersLargerThan(5, obj);
+//console.log(obj); // --> { b: 2, c: 'montana' }
+
+function removeNumbersLargerThan(num, obj) {
+  for (var key in obj){
+    if (obj[key] > num){
+      delete obj[key]
+    }
+  }
+  return obj;
+}
+
+//Write a function called "removeNumbersLessThan".
+//
+//Given a number and an object, "removeNumbersLessThan" removes any properties whose values are numbers less than the given number.
+//
+//var obj = {
+//  a: 8,
+//  b: 2,
+//  c: 'montana'
+//}
+//removeNumbersLessThan(5, obj);
+//console.log(obj); // --> { a: 8, c: 'montana' }
+
+function removeNumbersLessThan(num, obj) {
+  for (var key in obj){
+    if (obj[key] < num){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "removeStringValuesLongerThan".
+//
+//Given an number and an object, "removeStringValuesLongerThan" removes any properties on the given object whose values are strings longer than the given number.
+//
+//var obj = {
+//  name: 'Montana',
+//  age: 20,
+//  location: 'Texas'
+//};
+//removeStringValuesLongerThan(6, obj);
+//console.log(obj); // { age: 20, location: 'Texas' }
+
+function removeStringValuesLongerThan(num, obj) {
+  for (var key in obj){
+    if (obj[key].length > num){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "removeEvenValues".
+//
+//Given any object, "removeEvenValues" removes any properties whose values are even numbers.
+//
+//Do this in place and return the original object, do not construct a cloned object that omits the properties.
+//
+//var obj = {
+//  a: 2,
+//  b: 3,
+//  c: 4
+//};
+//removeEvenValues(obj);
+//console.log(obj); // --> { b: 3 }
+
+function removeEvenValues(obj) {
+  for (var key in obj){
+    if (obj[key] % 2 === 0){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "countNumberOfKeys".
+//
+//Given an object, "countNumberOfKeys" returns how many properties the given object has.
+//
+//var obj = {
+//  a: 1,
+//  b: 2,
+//  c: 3
+//};
+//var output = countNumberOfKeys(obj);
+//console.log(output); // --> 3
+
+function countNumberOfKeys(obj) {
+  return Object.keys(obj).length
+}
+
+//Write a function called "removeOddValues".
+//
+//Given an object, "removeOddValues" removes any properties whose valuse are odd numbers.
+//
+//var obj = {
+//  a: 2,
+//  b: 3,
+//  c: 4
+//};
+//removeOddValues(obj);
+//console.log(obj); // --> { a: 2, c: 4 }
+
+function removeOddValues(obj) {
+  for (var key in obj){
+    if (obj[key] % 2 !== 0){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "removeArrayValues".
+//
+//Given an object, "removeArrayValues" removes any properties whose values are arrays.
+//
+//var obj = {
+//  a: [1, 3, 4],
+//  b: 2,
+//  c: ['hi', 'there']
+//}
+//removeArrayValues(obj);
+//console.log(obj); // --> { b: 2 }
+
+function removeArrayValues(obj) {
+  for (var key in obj){
+    if (Array.isArray(obj[key])){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "removeNumberValues".
+//
+//Given an object, "removeNumberValues" removes any properties whose valuse are numbers.
+//
+//var obj = {
+//  a: 2,
+//  b: 'remaining',
+//  c: 4
+//};
+//removeNumberValues(obj);
+//console.log(obj); // --> { b: 'remaining' }
+
+function removeNumberValues(obj) {
+  for (var key in obj){
+    if (typeof obj[key] === 'number'){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "removeStringValues".
+//
+//Given an object, "removeStringValues" removes any properties on the given object whose values are strings.
+//
+//var obj = {
+//  name: 'Sam',
+//  age: 20
+//}
+//removeStringValues(obj);
+//console.log(obj); // { age: 20 }
+
+function removeStringValues(obj) {
+  for (var key in obj){
+    if (typeof obj[key] === 'string'){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+//Write a function called "convertDoubleSpaceToSingle".
+//
+//Given a string, "convertDoubleSpaceToSingle" returns the passed in string, with all the double spaces converted to single spaces.
+//
+//var output = convertDoubleSpaceToSingle("string  with  double  spaces");
+//console.log(output); // --> "string with double spaces"
+
+function convertDoubleSpaceToSingle(str) {
+  var strArr = str.split("  ");
+  var output = strArr.join(" ");
+  return output;
+}
